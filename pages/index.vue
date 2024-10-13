afe6dcea9caaed2d7cadda35d44d78062ac8a680
João Vitor <jvlima0002020@gmail.com>
2024-10-13 14:41:38 -0300

    base of project

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,12 @@
 <template>
   <main id="home-page">
-    <section v-if="index.length > 0">
       <div class="container">
         <div class="row">
           <h2>API</h2>
-          <p>{{ index[0].home.fields.title }}</p>
         </div>
       </div>
-    </section>
   </main>
-
 </template>
 
-<script setup>
-import { useIndex } from "../composables/useIndex";
-const { index } = useIndex();
-
-
-</script>
 
 <style src="@/assets/css/home.css" scoped></style>
